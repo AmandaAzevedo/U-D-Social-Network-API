5278e591981af3e0abd11c72704116ced3c83d18
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-07-29 19:06:04 -0300

    Created project

---
+++ src/main/java/tech/amandaazevedo/udsnetwork/api/UDSNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package tech.amandaazevedo.udsnetwork.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDSNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UDSNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/amandaazevedo/udsnetwork/api/UDSNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.amandaazevedo.udsnetwork.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UDSNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
