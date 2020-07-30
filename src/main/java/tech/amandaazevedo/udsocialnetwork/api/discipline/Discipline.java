325892b550907aadd9eba0d24bdd72dda78c2ac9
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-07-30 00:41:49 -0300

    Created Discipline domain class

---
+++ src/main/java/tech/amandaazevedo/udsocialnetwork/api/discipline/Discipline.java
@@ -0,0 +1,40 @@
+package tech.amandaazevedo.udsocialnetwork.api.discipline;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Setter
+@ToString
+@EqualsAndHashCode(of={"id"})
+@Entity
+public class Discipline {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "discipline_generator")
+    @SequenceGenerator(name = "discipline_generator", sequenceName = "discipline_sequence", allocationSize = 1)
+    private Long id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String name;
+
+    @NonNull
+    private double note;
+
+}
